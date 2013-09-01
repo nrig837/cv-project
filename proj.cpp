e3fb2e614c7a9f68ad6037211f4e0e4ab56cd6c2
Joe Barcham <joebarcham@gmail.com>
2013-09-01 22:09:05 +1000

    colour images, changed matching to knnMatch and tweaked parameters
    colour vs greyscale appears to make no difference to performance
    performance improved by around 20%
    fixed crash caused by insufficient points being passed to findHomography

--- proj.cpp
+++ proj.cpp
@@ -17,7 +17,7 @@ int main(int argc, char **argv) {
    } 
 
    // Read in target object and video file
-   Mat target_object = imread(argv[1], CV_LOAD_IMAGE_GRAYSCALE);
+   Mat target_object = imread(argv[1], CV_LOAD_IMAGE_COLOR);
 
    const string video_file = argv[2];
    VideoCapture capture(video_file);
@@ -29,7 +29,7 @@ int main(int argc, char **argv) {
    double start;
 
    // Setup SURF detector and precompute ketpoints for object
-    int minHessian = 400;
+    int minHessian = 1000;
     SurfFeatureDetector detector(minHessian);
     vector<KeyPoint> object_keypoints, frame_keypoints;
     detector.detect(target_object, object_keypoints);
@@ -41,14 +41,14 @@ int main(int argc, char **argv) {
 
     // Setup FLANN Matcher
     FlannBasedMatcher matcher;
-    vector<DMatch> matches;
+    vector< vector<DMatch> > matches;
 
    // Hunt for object in each video frame
    namedWindow("Object Matching", 1);
    Mat frame;
    while (capture.read(frame)) {
       start = getTime();
-      cvtColor(frame, frame, CV_BGR2GRAY);
+      //cvtColor(frame, frame, CV_BGR2GRAY);
 
       // Detect keypoints using SURF descriptor
       detector.detect(frame, frame_keypoints);
@@ -58,13 +58,12 @@ int main(int argc, char **argv) {
       
       // Match descriptors (using FLANN matcher)
       matches.clear(); //Delete previous matches
-      matcher.match(object_descriptors, frame_descriptors, matches);
-
+      matcher.knnMatch(object_descriptors, frame_descriptors, matches, 2);
       // Draw only the "good" matches (distance less than 3 * min_dist)
       // TODO: consider improvements (this is bit suspect/rough)
       double max_dist = 0;
       double min_dist = 100;
-      for (int i = 0; i < object_descriptors.rows; i++) { 
+      /*for (int i = 0; i < object_descriptors.rows; i++) { 
          double dist = matches[i].distance;
          if (dist < min_dist) min_dist = dist;
          if (dist > max_dist) max_dist = dist;
@@ -72,42 +71,52 @@ int main(int argc, char **argv) {
       vector<DMatch> good_matches;
       for (int i = 0; i < object_descriptors.rows; i++)
          if (matches[i].distance < 3 * min_dist)
-	    good_matches.push_back(matches[i]);
+	        good_matches.push_back(matches[i]);*/
+      vector<DMatch> good_matches;
+      for(int k = 0; k < matches.size(); k++) {
+          if(matches[k].size() == 2 && matches[k][0].distance < 0.7*(matches[k][1].distance))
+              good_matches.push_back(matches[k][0]);
+      }
       Mat img_matches;
 
       drawMatches(target_object, object_keypoints, frame, frame_keypoints,
                   good_matches, img_matches, Scalar::all(-1), Scalar::all(-1),
                   vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS);
 
-      // Actually find object in frame (work out pose + location)
-      vector<Point2f> object_points, frame_points;
-      // Only take keypoints from the "good" matches
-      for (int i = 0; i < good_matches.size(); i++) {
-         object_points.push_back(object_keypoints[good_matches[i].queryIdx].pt);
-         frame_points.push_back(frame_keypoints[good_matches[i].trainIdx].pt);
+      if (good_matches.size() < 4) {
+         cout << good_matches.size() << " good matches, skipping frame" << endl;
+      } else {
+         cout << good_matches.size() << " good matches" << endl;
+
+         // Actually find object in frame (work out pose + location)
+         vector<Point2f> object_points, frame_points;
+         // Only take keypoints from the "good" matches
+         for (int i = 0; i < good_matches.size(); i++) {
+            object_points.push_back(object_keypoints[good_matches[i].queryIdx].pt);
+            frame_points.push_back(frame_keypoints[good_matches[i].trainIdx].pt);
+         }
+         // Compute homography matrix, which gives the mapping of the query
+         // (planar) object to the frame (i.e. coordinate system mapping)
+         Mat H = findHomography(object_points, frame_points, CV_RANSAC);
+         // Get corners of the query object (object to be detected)
+         vector<Point2f> object_corners(4);
+         object_corners[0] = cvPoint(0, 0); 
+         object_corners[1] = cvPoint(target_object.cols, 0);
+         object_corners[2] = cvPoint(target_object.cols, target_object.rows); 
+         object_corners[3] = cvPoint(0, target_object.rows);
+         vector<Point2f> frame_corners(4);
+         perspectiveTransform(object_corners, frame_corners, H);
+
+         // Draw the actual lines around the detected object
+         line(img_matches, frame_corners[0] + Point2f(target_object.cols, 0), 
+               frame_corners[1] + Point2f(target_object.cols, 0), Scalar(0, 255, 0), 4);
+         line(img_matches, frame_corners[1] + Point2f(target_object.cols, 0), 
+               frame_corners[2] + Point2f(target_object.cols, 0), Scalar( 0, 255, 0), 4);
+         line(img_matches, frame_corners[2] + Point2f(target_object.cols, 0), 
+               frame_corners[3] + Point2f(target_object.cols, 0), Scalar( 0, 255, 0), 4);
+         line(img_matches, frame_corners[3] + Point2f(target_object.cols, 0), 
+               frame_corners[0] + Point2f(target_object.cols, 0), Scalar( 0, 255, 0), 4);
       }
-      // Compute homography matrix, which gives the mapping of the query
-      // (planar) object to the frame (i.e. coordinate system mapping)
-      Mat H = findHomography(object_points, frame_points, CV_RANSAC);
-      // Get corners of the query object (object to be detected)
-      vector<Point2f> object_corners(4);
-      object_corners[0] = cvPoint(0, 0); 
-      object_corners[1] = cvPoint(target_object.cols, 0);
-      object_corners[2] = cvPoint(target_object.cols, target_object.rows); 
-      object_corners[3] = cvPoint(0, target_object.rows);
-      vector<Point2f> frame_corners(4);
-      perspectiveTransform(object_corners, frame_corners, H);
-
-      // Draw the actual lines around the detected object
-      line(img_matches, frame_corners[0] + Point2f(target_object.cols, 0), 
-           frame_corners[1] + Point2f(target_object.cols, 0), Scalar(0, 255, 0), 4);
-      line(img_matches, frame_corners[1] + Point2f(target_object.cols, 0), 
-           frame_corners[2] + Point2f(target_object.cols, 0), Scalar( 0, 255, 0), 4);
-      line(img_matches, frame_corners[2] + Point2f(target_object.cols, 0), 
-           frame_corners[3] + Point2f(target_object.cols, 0), Scalar( 0, 255, 0), 4);
-      line(img_matches, frame_corners[3] + Point2f(target_object.cols, 0), 
-           frame_corners[0] + Point2f(target_object.cols, 0), Scalar( 0, 255, 0), 4);
-
       // Add frame timer to image
       putText(img_matches, format("%0.0fms",getTime()-start),Point(frame.cols-10,20), FONT_HERSHEY_PLAIN, 1.0, CV_RGB(0,255,0), 1);
 
