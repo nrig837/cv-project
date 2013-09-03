c0caa3ff18aa93e9c3cae92783a49838d0a37ee6
Nelson Rigby <nelsonrigby@Nelsons-MacBook-Pro.local>
2013-09-03 20:36:52 +1000

    Displayed target images beside video, added auto-adjust of hessian threshold, increased good matches threshold

--- proj.cpp
+++ proj.cpp
@@ -1,6 +1,7 @@
 #include "opencv2/opencv.hpp"
 #include "opencv2/nonfree/features2d.hpp" 
 #include <iostream>
+#include <algorithm>
 #include <vector>
 
 using namespace cv;
@@ -35,8 +36,12 @@ int main(int argc, char **argv) {
    }
 
    // Setup SURF detector and precompute ketpoints for objects
-   int minHessian = 400;//1000; // Larger = faster, worse matching. Smaller = slower, better matching
-   SurfFeatureDetector detector(minHessian);
+   int hessianThresh = 500;//200;//1000; // Larger = faster, worse matching. Smaller = slower, better matching
+   // Setup some playback parameters (related to detector parameters)
+   double average_fps = 0.0;
+   double DESIRED_FPS = 15.0;
+   int tooSlow = -1;
+   SurfFeatureDetector detector(hessianThresh);
    vector<KeyPoint> frame_keypoints;
    vector< vector<KeyPoint> > object_keypoints_list;
    for(uint i = 0; i < NUM_TARGETS; ++i) {
@@ -58,17 +63,99 @@ int main(int argc, char **argv) {
 
    // Misc setup
    double start;
-   namedWindow("Object Matching", 1);
+   namedWindow("Object Matching", CV_WINDOW_AUTOSIZE);
    Mat frame;
    double temp_start = getTime();
    int num_frames = 0;
+   Mat img_out, img_matches;
+   int x = 0, y = 0;
    // Hunt for object in each video frame
    while (capture.read(frame)) {
       num_frames++;
       start = getTime();
       //cvtColor(frame, frame, CV_BGR2GRAY);
-
+      // Set size of output matrix if needed
+      if (img_out.empty() && x == 0) {
+         // Attach other target objects to the left side
+         int widthPad = 0;
+         int maxWidth = 0;
+	 int maxHeight = 0;
+         for (int i = 0; i < NUM_TARGETS; i++) {
+	    maxWidth = max(maxWidth, target_list[i].cols);
+	    maxHeight = max(maxHeight, target_list[i].rows);
+	    cout << "maxWidth: " << maxWidth << endl;
+         } 
+	 // Add a little extra for safety
+	 widthPad += maxWidth * NUM_TARGETS;
+         // Create expanded matrix to store objects on the side
+         img_out.create(max(frame.rows, maxHeight), frame.cols + widthPad, 
+			frame.type());
+         for (int i = 1; i < NUM_TARGETS; i++) {
+	    if (y + target_list[i].rows > img_out.rows) {
+               x += maxWidth;
+	       y = 0;
+	    }
+	    cout << "Placing " << i << "th image at x: " << x << endl;
+   	    Mat displayRegion(img_out, Rect(x, y, target_list[i].cols, 
+   				            target_list[i].rows));
+	    y += target_list[i].rows;
+            target_list[i].copyTo(displayRegion);
+         }
+	 // Messy
+	 x += target_list[NUM_TARGETS-1].cols; //widthPad - target_list[0].cols;
+	 cout << "rest will start from x: " << x << endl;
+      }
       // Detect keypoints using SURF descriptor
+      // (adapt hessian threshold based on running average of frame rate)
+      double cur_time = getTime() - temp_start;
+      average_fps = (double) num_frames / (cur_time / 1000.0);
+      bool stable = (num_frames > 15);
+      if (stable && tooSlow == -1) {
+         tooSlow = (average_fps < DESIRED_FPS);
+	 cout << "tooSlow set to: " << tooSlow << endl;
+      }
+
+      bool recompute = false;
+      int minHessian = 50, maxHessian = 1000;
+      cout << "tooSlow: " << tooSlow << " average_fps: " << average_fps << endl;
+      if (stable && tooSlow == 1 && !(num_frames % 10) && 
+          average_fps < DESIRED_FPS && 
+	  minHessian < hessianThresh &&
+	  maxHessian > hessianThresh) {
+	 cout << "Increasing threshold (speeding up)..." << endl;
+         hessianThresh += 50;
+	 tooSlow = 1; 
+	 recompute = true;
+      } else if (stable && !tooSlow && !(num_frames % 10) && 
+		 average_fps > DESIRED_FPS &&
+		 minHessian < hessianThresh &&
+		 maxHessian > hessianThresh) {
+	 cout << "Decreasing threshold (slowing down)..." << endl;
+	 hessianThresh -= 50;
+	 tooSlow = 0; 
+	 recompute = true;
+      } else {
+	 recompute = false;
+      }
+      if (recompute) {
+	 cout << "Recomputing: " << hessianThresh << endl;
+         SurfFeatureDetector detector_new(hessianThresh);
+	 detector = detector_new;
+         vector<KeyPoint> frame_keypoints;
+         vector< vector<KeyPoint> > object_keypoints_list;
+         for(uint i = 0; i < NUM_TARGETS; ++i) {
+            vector<KeyPoint> kp;
+            detector.detect(target_list[i], kp);
+            object_keypoints_list.push_back(kp);
+         }
+   
+         // Setup decriptors and precompute for objects
+         SurfDescriptorExtractor extractor;
+         Mat frame_descriptors;
+         vector<Mat> object_descriptors_list(NUM_TARGETS);
+         for(uint i = 0; i < NUM_TARGETS; ++i)
+            extractor.compute(target_list[i], object_keypoints_list[i], object_descriptors_list[i]);
+      }
       frame_keypoints.clear();
       detector.detect(frame, frame_keypoints);
       
@@ -114,7 +201,6 @@ int main(int argc, char **argv) {
          good_match_list.push_back(gm_tmp);
       }
 
-      Mat img_matches;
       
       // Only draws matches for first image, the rest just get lines
       drawMatches(target_list[0], object_keypoints_list[0], frame, frame_keypoints,
@@ -126,7 +212,7 @@ int main(int argc, char **argv) {
          vector<KeyPoint>& object_keypoints = object_keypoints_list[obj];
          Mat& target_object = target_list[obj];
 
-         if (good_matches.size() < 4) {
+         if (good_matches.size() < 8) {
             cout << obj << ": " << good_matches.size() << " good matches, skipping image" << endl;
          } else {
             cout << obj << ": " << good_matches.size() << " good matches" << endl;
@@ -164,8 +250,13 @@ int main(int argc, char **argv) {
       }
       // Add frame timer to image
       putText(img_matches, format("%0.0fms",getTime()-start),Point(frame.cols-60,20), FONT_HERSHEY_PLAIN, 1.0, CV_RGB(0,255,0), 1);
+      cout << "x: " << x << " img_matches.cols: " << img_matches.cols << endl;
+      cout << "img_out cols: " << img_out.cols << endl;
+      Mat displayRest(img_out, Rect(x, 0, img_matches.cols, 
+			            img_matches.rows));
+      img_matches.copyTo(displayRest);
 
-      imshow("Object Matching", img_matches);
+      imshow("Object Matching", img_out);
       if(waitKey(1) >= 0) break;
    }
 
